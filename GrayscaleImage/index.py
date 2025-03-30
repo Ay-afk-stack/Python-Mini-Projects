009f7f362e02231c5c2a5d71753c58947bc55917
ay-afk-stack <pakhrin.ayush404>
2025-03-30 11:31:14 +0545

    added upload file

--- GrayscaleImage/index.py
+++ GrayscaleImage/index.py
@@ -1,6 +1,11 @@
 import streamlit as st
 from PIL import Image
 
+st.subheader("Color to Grayscale Converter")
+
+upload_image = st.file_uploader("Upload Image")
+
+
 with st.expander("Start Camera"):
     #Start The Camera
     camera_image = st.camera_input("Camera")
@@ -14,3 +19,14 @@ with st.expander("Start Camera"):
 
         #Render the grayscale image on the webpage
         st.image(gray_img)
+
+if upload_image:
+        #Create a pillow image instance
+        img = Image.open(upload_image)
+
+        #Convert image to grayscale
+        gray_img = img.convert("L")
+
+        #Render the grayscale image on the webpage
+        st.image(gray_img)
+        
